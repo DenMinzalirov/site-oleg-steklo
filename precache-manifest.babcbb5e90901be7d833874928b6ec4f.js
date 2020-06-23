9466cc7bdde273b3a7e7f41dd1725720964fb57b
DenMinzalirov <den.minzakirov@gmail.com>
2020-06-23 11:46:25 +0300

    0.1

---
+++ precache-manifest.babcbb5e90901be7d833874928b6ec4f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e09c098cdd93e69597da1eb2c86ee624",
+    "url": "/site-oleg-steklo/index.html"
+  },
+  {
+    "revision": "6311a066fd728a795185",
+    "url": "/site-oleg-steklo/static/css/2.9f2bb942.chunk.css"
+  },
+  {
+    "revision": "e1b5fcb15262ad90c5c1",
+    "url": "/site-oleg-steklo/static/css/main.d743ee44.chunk.css"
+  },
+  {
+    "revision": "6311a066fd728a795185",
+    "url": "/site-oleg-steklo/static/js/2.f75050aa.chunk.js"
+  },
+  {
+    "revision": "ba5c44dd1ae0cf4a36ce821f0b09a39e",
+    "url": "/site-oleg-steklo/static/js/2.f75050aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1b5fcb15262ad90c5c1",
+    "url": "/site-oleg-steklo/static/js/main.b751a346.chunk.js"
+  },
+  {
+    "revision": "62e312dd1533dd80ffbd",
+    "url": "/site-oleg-steklo/static/js/runtime-main.04b8a829.js"
+  },
+  {
+    "revision": "4070a4de6a3113a2963d7dab08cc76bd",
+    "url": "/site-oleg-steklo/static/media/19516.4070a4de.otf"
+  }
+]);
